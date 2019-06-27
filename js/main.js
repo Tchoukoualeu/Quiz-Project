e71ae77f339c2291ec4d480dde4129dd09d39d14
Aris Tchoukoualeu <38705757+Tchoukoualeu@users.noreply.github.com>
2019-06-27 19:22:44 +0200

    'MyUpdate'

--- js/main.js
+++ js/main.js
@@ -6,47 +6,47 @@ function toSecond(){
 }
 
 function toThird(){
-    document.getElementById('second').style.display = 'none';
+    document.getElementById('second').classList = 'card shadow-lg border-primary mb-3 animated bounceOutRight';
     document.getElementById('third').style.display = 'block';
 }
 
 function toFourth(){
-    document.getElementById('third').style.display = 'none';
+    document.getElementById('third').classList = 'card shadow-lg border-primary mb-3 animated bounceOutRight';
     document.getElementById('fourth').style.display = 'block';
 }
 
 function toFifth(){
-    document.getElementById('fourth').style.display = 'none';
+    document.getElementById('fourth').classList = 'card shadow-lg border-primary mb-3 animated bounceOutRight';
     document.getElementById('fifth').style.display = 'block';
 }
 
 function toSixth(){
-    document.getElementById('fifth').style.display = 'none';
+    document.getElementById('fifth').classList = 'card shadow-lg border-primary mb-3 animated bounceOutRight';
     document.getElementById('sixth').style.display = 'block';
 }
 
 function toSeventh(){
-    document.getElementById('sixth').style.display = 'none';
+    document.getElementById('sixth').classList = 'card shadow-lg border-primary mb-3 animated bounceOutRight';
     document.getElementById('seventh').style.display = 'block';
 }
 
 function toEighth(){
-    document.getElementById('seventh').style.display = 'none';
+    document.getElementById('seventh').classList = 'card shadow-lg border-primary mb-3 animated bounceOutRight';
     document.getElementById('eighth').style.display = 'block';
 }
 
 function toNinth(){
-    document.getElementById('eighth').style.display = 'none';
+    document.getElementById('eighth').classList = 'card shadow-lg border-primary mb-3 animated bounceOutRight';
     document.getElementById('ninth').style.display = 'block';
 }
 
 function toTenth(){
-    document.getElementById('ninth').style.display = 'none';
+    document.getElementById('ninth').classList = 'card shadow-lg border-primary mb-3 animated bounceOutRight';
     document.getElementById('tenth').style.display = 'block';
 }
 
 function toFinal(){
-    document.getElementById('tenth').style.display = 'none';
+    document.getElementById('tenth').classList = 'card shadow-lg border-primary mb-3 animated bounceOutRight';
     // document.getElementById('final').style.display = 'block';
 }
 
